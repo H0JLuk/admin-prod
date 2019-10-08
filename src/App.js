bd208f680a9c97b6c14f7a901851fed66e3eac80
megakaka <zolotenkov.i.ni@sberbank.ru>
2019-10-08 12:49:01 +0300

    fix no middlename bug

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import  {Button}  from 'megakaka-web-ui';
+import * as styles from './App.css';
+
+class App extends React.PureComponent {
+
+    render() {
+
+        console.log('btn', Button);
+        return (<div className="App">
+            asdas
+            <Button type="button"/>
+        </div>)
+    }
+}
+
+export default App;
