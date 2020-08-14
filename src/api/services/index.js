76b74b370b0374b348697e1ce54d0b2cd21ae255
Запрягаев Александр Евгеньевич <AEvZapryagaev@sberbank.ru>
2020-08-14 13:12:32 +0300

    request header added only if it's value is not null

--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,9 +1,13 @@
 import { getAppCode, getSession } from './sessionService';
 
+const TOKEN_HEADER = 'token';
+const APP_CODE_HEADER = 'clientAppCode';
+
 export const getReqOptions = () => {
     const token = getSession();
-    const clientAppCode = getAppCode();
-    return {
-        headers: { token, clientAppCode }
-    };
+    const appCode = getAppCode();
+    const headers = {};
+    token && (headers[TOKEN_HEADER] = token);
+    appCode && (headers[APP_CODE_HEADER] = appCode);
+    return { headers };
 };
