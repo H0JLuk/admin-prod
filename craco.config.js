a89cbf78e056c066c65bdbb3a8f7d83bcdb898ed
Лучко Дмитрий Дмитриевич <DDLuchko@sberbank.ru>
2021-03-03 16:50:34 +0300

    Pull request #156: add PUBLIC_URL in craco config, not generate source map for production, max chunk size < 1MB
    Squashed commit of the following:
    
    commit 591c99c78d922107691b423330a64658684d0351
    Author: OUT-Luchko-DD <DDLuchko@sberbank.ru>
    Date:   Wed Mar 3 14:55:25 2021 +0300
    
        add PUBLIC_URL in craco config
        not generate source map for production
        max chunk size < 1MB

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,17 @@
+const isProd = process.env.NODE_ENV === 'production';
+process.env.PUBLIC_URL = isProd ? '/admin/' : '/';
+process.env.GENERATE_SOURCEMAP = isProd ? 'false' : 'true';
+
 module.exports = {
     webpack: {
         configure: {
             output: {
-                publicPath: process.env.NODE_ENV === 'production' ? '/admin/' : '/',
+                publicPath: process.env.PUBLIC_URL,
+            },
+            optimization: {
+                splitChunks: {
+                    maxSize: 1024 * 1000,
+                },
             },
         },
     },
