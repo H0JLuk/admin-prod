eb1ae1f4cddb74adad09a6854ee352b3b72ae061
Лучко Дмитрий Дмитриевич <DDLuchko@sberbank.ru>
2021-03-16 17:46:37 +0300

    Pull request #171: WDZO-1644: fix url regex
    Squashed commit of the following:
    
    commit 116fc0d7779c85df1158aeabf971b21835a1ceb4
    Author: OUT-Luchko-DD <DDLuchko@sberbank.ru>
    Date:   Tue Mar 16 17:29:59 2021 +0300
    
        WDZO-1644: fix url regex

--- src/constants/tests/urlRegExpTest.test.js
+++ src/constants/tests/urlRegExpTest.test.js
@@ -8,6 +8,10 @@ test('valid url test', () => {
     expect(URL_REGEXP.test('https://www.sberbank.com/?test1=test1&test2=[test2]&test3={test3}')).toBe(true);
     expect(URL_REGEXP.test('https://promo.sberhealth.ru/vvo')).toBe(true);
     expect(URL_REGEXP.test('http://promo.sberhealth.ru/vvo')).toBe(true);
+    expect(URL_REGEXP.test('http://promo.sberhealth.ru/vVo')).toBe(true);
+    expect(URL_REGEXP.test('https://sbol.onelink.me/INEy/89af6cd1')).toBe(true);
+    expect(URL_REGEXP.test('https://sbol.onelink.me/INEy/89aTESTcd1#test?eeee=Er')).toBe(true);
+    expect(URL_REGEXP.test('http://promo.sberhealth.ru/vVo/teSSS')).toBe(true);
     expect(URL_REGEXP.test('http://promo.sberhealth.ru/?vvo')).toBe(true);
     expect(URL_REGEXP.test('http://promo.sberhealth.ru?vvo')).toBe(true);
     expect(URL_REGEXP.test('https://www.sberbank.ru/?source=vitrina.sber.ru&offer_client={OFFER_ID}&code_client={PROMO_CODE}&hash={PHONE_NUMBER_HASH}&phone={PHONE_NUMBER}&uid={USER_ID}')).toBe(true);
@@ -18,6 +22,7 @@ test('valid url test', () => {
     expect(URL_REGEXP.test('https://vml8.adj.st/app?adj_t=caih37y&adj_campaign=partner_sberbank&adj_adgroup=partner_sberbank_ecosystem&adj_creative=partner_sberbank_ecosystem_vitrina_2009')).toBe(true);
     expect(URL_REGEXP.test('https://okko.tv/#promo-code/?test=13&tyr=223')).toBe(true);
     expect(URL_REGEXP.test('https://okko.tv/#promo-code?test=13&tyr=223')).toBe(true);
+    expect(URL_REGEXP.test('https://okko.tv/#prOMOo-code?tTsT=13&tYr=2R23')).toBe(true);
 });
 
 test('not valid url test', () => {
